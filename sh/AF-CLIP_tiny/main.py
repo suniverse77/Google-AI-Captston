155088f834cc4f119f9e0d340a634534865cf409
suno <suniverse2480@gmail.com>
2025-11-08 15:10:07 +0900

    update

--- sh/AF-CLIP_tiny/main.py
+++ sh/AF-CLIP_tiny/main.py
@@ -14,7 +14,7 @@ from torchvision import transforms
 
 from dataset import *
 from util.utils import eval_all_class
-from util.loss_fn import focal_loss, l1_loss
+from util.loss_fn import focal_loss, l1_loss, patch_alignment_loss
 from clip.clip import load, tokenize, available_models
 
 
@@ -53,26 +53,6 @@ def print_args(logger, args):
     for k in list(vars(args).keys()):
         logger.info('{}: {}'.format(k, vars(args)[k]))
     logger.info('--------args----------\n')
-
-def patch_alignment_loss(img_tokens, labels, gts):
-    gts = gts.reshape(img_tokens[0].size(0), -1)
-    labels = labels.reshape(labels.size(0), 1)
-    # labels = torch.cat([labels, gts], dim=1)
-    new_gts = copy.copy(gts)
-    if(len(new_gts[new_gts == 0])) == 0:
-        return 0
-    new_gts[new_gts == 0] = -1
-    b, l = new_gts.size()
-    mask = torch.matmul(new_gts.reshape(b, l, 1), new_gts.reshape(b, 1, l))
-    total_sim = 0
-    for img_token in img_tokens:
-        img_token = img_token[:, 1:, :]
-        img_token = torch.nn.functional.normalize(img_token, dim=-1)
-        sim = torch.matmul(img_token, img_token.permute(0, 2, 1))
-        sim = sim[mask == -1].mean() - sim[mask == 1].mean()
-        sim = sim if sim > 0 else 0
-        total_sim = total_sim + sim
-    return total_sim / len(img_tokens)
     
 
 def train(args):
--- sh/AF-CLIP_tiny/util/loss_fn.py
+++ sh/AF-CLIP_tiny/util/loss_fn.py
@@ -1,3 +1,5 @@
+import copy
+
 import torch
 import torch.nn.functional as F
 
@@ -21,3 +23,24 @@ def focal_loss(inputs, targets, alpha=-1, gamma=4, reduction="mean"):
 
 def l1_loss(inputs, targets, reduction="mean"):
     return F.l1_loss(inputs, targets, reduction=reduction)
+
+def patch_alignment_loss(img_tokens, labels, gts):
+    gts = gts.reshape(img_tokens[0].size(0), -1)
+    labels = labels.reshape(labels.size(0), 1)
+    # labels = torch.cat([labels, gts], dim=1)
+    new_gts = copy.copy(gts)
+    if(len(new_gts[new_gts == 0])) == 0:
+        return 0
+    new_gts[new_gts == 0] = -1
+    b, l = new_gts.size()
+    mask = torch.matmul(new_gts.reshape(b, l, 1), new_gts.reshape(b, 1, l))
+    total_sim = 0
+    for img_token in img_tokens:
+        img_token = img_token[:, 1:, :]
+        img_token = torch.nn.functional.normalize(img_token, dim=-1)
+        sim = torch.matmul(img_token, img_token.permute(0, 2, 1))
+        sim = sim[mask == -1].mean() - sim[mask == 1].mean()
+        sim = sim if sim > 0 else 0
+        total_sim = total_sim + sim
+        
+    return total_sim / len(img_tokens)
